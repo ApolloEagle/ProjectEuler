a121f3a750a047ed59f018163133d7e41bd8060b
Blake Reimer <sobeksea@gmail.com>
2018-06-12 23:45:39 -0600

    Add function to PrimeSum solution

--- PrimeSum.py
+++ PrimeSum.py
@@ -1,11 +1,12 @@
-n = 2000000
-prime = []
+def primeSum(n):
+    prime = []
 
-for p in range(2, n + 1):
-    for i in range(2, int(p ** 0.5) + 1):
-        if p % i == 0:
-            break
-    else:
-        prime.append(p)
+    for p in range(2, n + 1):
+        for i in range(2, int(p ** 0.5) + 1):
+            if p % i == 0:
+                break
+        else:
+            prime.append(p)
+    return sum(prime)
 
-print(sum(prime))
+print(primeSum(2000000))
