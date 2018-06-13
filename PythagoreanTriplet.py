e07bea9ab6d6f61a866749e169e6c86d01f871fb
Blake Reimer <sobeksea@gmail.com>
2018-06-12 23:48:16 -0600

    Add function function to Pythagorean Triplet solution

--- PythagoreanTriplet.py
+++ PythagoreanTriplet.py
@@ -1,7 +1,9 @@
-n = 1000
+def pythagoreanTriplet(n):
 
-for a in range(1, n + 1):
-    for b in range(a, n + 1):
-        c = n - a - b
-        if a**2 + b**2 == c**2:
-            print(a * b * c)
+    for a in range(1, n + 1):
+        for b in range(a, n + 1):
+            c = n - a - b
+            if a**2 + b**2 == c**2:
+                return (a * b * c)
+
+print(pythagoreanTriplet(1000))
