8d2f67fa622a732d39b37bd60cf446316c1edac6
Blake Reimer <sobeksea@gmail.com>
2018-06-12 23:15:15 -0600

    Initial commit

---
+++ LargestPrimeFactor.py
@@ -0,0 +1,11 @@
+def largestPrimeFactor(n):
+
+    i = 2
+    while i * i <= n:
+        if n % i:
+            i += 1
+        else:
+            n //= i
+    return n
+
+print(largestPrimeFactor(600851475143))
